70f010bb9dd66a2e613c737da29a48b82a7a1624
Annibal Biondi <annibal.biondi@gmail.com>
2014-11-18 01:22:22 -0200

    added static stuff to urls.py

--- poc_project/urls.py
+++ poc_project/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
@@ -10,4 +12,4 @@ urlpatterns = patterns(
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^reader/', include('reader.urls')),
-    )
+    ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
