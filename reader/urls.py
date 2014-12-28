89494e6dc57673e51098d80a22a0b337abffb976
Annibal Biondi <annibal.biondi@gmail.com>
2014-12-28 05:03:49 -0200

    created the user home page

--- reader/urls.py
+++ reader/urls.py
@@ -4,7 +4,6 @@ from reader import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.index, name='index'),
-    url(r'^user', views.user_home, name='user_home'),
     url(r'^click', views.click, name='click'),
     url(r'^dev', views.dev, name='dev'),
     url(r'^feed', views.feed_page, name='feed_page'),
