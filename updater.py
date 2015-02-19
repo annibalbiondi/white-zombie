925ea3dac93811ad2a663d19f28a706ae4427ff1
Annibal Biondi <annibal.biondi@gmail.com>
2015-02-19 15:29:29 -0200

    updater fix

--- updater.py
+++ updater.py
@@ -1,5 +1,5 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','poc_project.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE','poc_project.settings')
 from reader import rss
 
 rss.update_feeds()
