a8608af536970f8a23b7597668bb61268f4c6367
Annibal Biondi <annibal.biondi@gmail.com>
2015-02-19 14:48:38 -0200

    notícias recomendadas passam a aparecer em negrito

--- precision_recall_test.py
+++ precision_recall_test.py
@@ -2,55 +2,74 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE','poc_project.settings')
 import codecs
 import datetime
+import random
 from reader.models import *
 from reader.classification import train_nb, classify
 
-# delimitacoes do periodo de treinamento
-start_date = datetime.datetime(2014, 2, 9)
-end_date = datetime.datetime(2014, 2, 10)
+# delimitacoes do periodo de treinamento e classificacao
+training_start = datetime.datetime(2014, 2, 9)
+training_end = datetime.datetime(2014, 2, 10)
+classification_start = datetime.datetime(2014, 2, 10)
+classification_end = datetime.datetime(2014, 2, 12)
 
-# extrair notícias a serem classificadas
+# extrair noticias a serem classificadas
 # serao classificadas todas as noticias posteriores ao periodo de treinamento
-feed_list = Feed.objects.filter(address__in((
+training_feeds = {}
+training_feeds['sports'] = Feed.objects.filter(address__in=(
+    'http://globoesporte.globo.com/servico/semantica/editorias/plantao/feed.rss',
+    'http://rss.uol.com.br/feed/noticias.xml',
+    'http://g1.globo.com/dynamo/rss2.xml'))
+training_feeds['economy'] = Feed.objects.filter(address__in=(
+    'http://rss.uol.com.br/feed/noticias.xml',
     'http://g1.globo.com/dynamo/rss2.xml',
     'http://g1.globo.com/dynamo/economia/rss2.xml',
-    'http://rss.uol.com.br/feed/noticias.xml',
-    'http://rss.uol.com.br/feed/economia.xml',
-    'http://globoesporte.globo.com/servico/semantica/editorias/plantao/feed.rss',
-    'http://esporte.uol.com.br/ultimas/index.xml')))
-     
-# TODO definir se serao classificadas noticias de todos os feeds
-# ou apenas aquelas dos feeds gerais
-classification_set = Entry.objects.filter(feed__in=feed_list,
-                                          pub_date__gt=end_date))
+    'http://rss.uol.com.br/feed/economia.xml'))
+
+classification_feeds = Feed.objects.filter(
+    address__in=('http://g1.globo.com/dynamo/rss2.xml',
+                 'http://rss.uol.com.br/feed/noticias.xml',
+                 'http://globoesporte.globo.com/servico/semantica/editorias/plantao/feed.rss',
+                 'http://rss.uol.com.br/feed/economia.xml'))
+
+classification_set = []
+
+for feed in classification_feeds:
+    classification_set.extend(random.sample(
+        Entry.objects.filter(
+            feed__in=classification_feeds,
+            ).exclude(pub_date__day=9),
+        30))
 
 for username in ('sports', 'economy'):
     reader_user = ReaderUser.objects.get(user__username=username)
     feeds = reader_user.feeds
-    discarded_news = ReceivedEntry.objects.exclude(
-        entry__pub_date__range(start_date, end_date))
-
+    training_news = Entry.objects.filter(
+        feed__in=training_feeds[username],
+        pub_date__day=10)
+    ignored_news = (Entry.objects.filter(
+        feed__in=training_feeds[username]).exclude(
+            pub_date__day=10))
     classifier = train_nb(
-        reader_user,
-        list(discarded_news).extend(
-            ReceivedEntry.objects.filter(entry__in=classification_set))
+        reader_user, ignored_news)
 
     results = []
 
     for entry in classification_set:
         result = classify(entry, classifier)
-        results.add((entry, result))
+        results.append((entry, result))
 
     # imprimir os resultados em um arquivo
     with codecs.open(
             'resultados.txt',
-            'w',
+            'a',
             encoding='utf-8') as results_file:
 
         results_file.write(username + ':')
-        results_file.write()
-        results_file.write()
+        results_file.write('\n')
+        results_file.write('\n')
         for entry, result in results:
-            results_file.write(entry.title + ' ' + result)
-            results_file.write()
-        results_file.write()
+            results_file.write(entry.title + ' ' + result + '\n' +
+                               entry.pub_date.isoformat(' ') + '\n' +
+                               entry.link + '\n')
+            results_file.write('\n')
+        results_file.write('\n')
