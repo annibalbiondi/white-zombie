56f325b83b785e3ea47e87716ec8c696a3cd0df5
Annibal Biondi <annibal.biondi@gmail.com>
2015-04-30 20:46:39 -0300

    dados de testes adicionados

--- precision_recall_test.py
+++ precision_recall_test.py
@@ -6,6 +6,8 @@ import random
 from reader.models import *
 from reader.classification import train_nb, classify
 
+iterations = 3
+
 # delimitacoes do periodo de treinamento e classificacao
 training_start = datetime.datetime(2014, 2, 9)
 training_end = datetime.datetime(2014, 2, 10)
@@ -31,45 +33,43 @@ classification_feeds = Feed.objects.filter(
                  'http://globoesporte.globo.com/servico/semantica/editorias/plantao/feed.rss',
                  'http://rss.uol.com.br/feed/economia.xml'))
 
-classification_set = []
+for i in range(0, iterations):
+    classification_set = []
 
-for feed in classification_feeds:
-    classification_set.extend(random.sample(
-        Entry.objects.filter(
-            feed__in=classification_feeds,
+    for feed in classification_feeds:
+        classification_set.extend(random.sample(
+            Entry.objects.filter(
+                feed__in=classification_feeds,
             ).exclude(pub_date__day=9),
-        30))
+            30))
 
-for username in ('sports', 'economy'):
-    reader_user = ReaderUser.objects.get(user__username=username)
-    feeds = reader_user.feeds
-    training_news = Entry.objects.filter(
-        feed__in=training_feeds[username],
-        pub_date__day=10)
-    ignored_news = (Entry.objects.filter(
-        feed__in=training_feeds[username]).exclude(
-            pub_date__day=10))
-    classifier = train_nb(
-        reader_user, ignored_news)
-
-    results = []
-
-    for entry in classification_set:
-        result = classify(entry, classifier)
-        results.append((entry, result))
-
-    # imprimir os resultados em um arquivo
     with codecs.open(
             'resultados.txt',
             'a',
             encoding='utf-8') as results_file:
+        results_file.write(str(i) + '\n')
+        for username in ('sports', 'economy'):
+            reader_user = ReaderUser.objects.get(user__username=username)
+            training_news = Entry.objects.filter(
+                feed__in=training_feeds[username],
+                pub_date__day=10)
+            ignored_news = (Entry.objects.filter(
+                feed__in=training_feeds[username]).exclude(
+                    pub_date__day=10))
+            classifier = train_nb(
+                reader_user, ignored_news)
 
-        results_file.write(username + ':')
-        results_file.write('\n')
-        results_file.write('\n')
-        for entry, result in results:
-            results_file.write(entry.title + ' ' + result + '\n' +
-                               entry.pub_date.isoformat(' ') + '\n' +
-                               entry.link + '\n')
+            results = []
+
+            for entry in classification_set:
+                result = classify(entry, classifier)
+                results.append((entry, result))
+
+            results_file.write(username + ':')
             results_file.write('\n')
-        results_file.write('\n')
+
+            # imprimir os resultados em um arquivo
+            for entry, result in results:
+                results_file.write(str(entry.id) + '^' + entry.title + '^' +
+                                   entry.link + '^' + result)
+                results_file.write('\n')
